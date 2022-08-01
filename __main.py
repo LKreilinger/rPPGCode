dada01b5ea845d47732caa9f3b8dc0e6b9cff8d8
Laurens Kreilinger <laurens.kreilinger@web.de>
2022-08-01 16:34:41 +0200

    Update

--- __main.py
+++ __main.py
@@ -20,48 +20,54 @@ if __name__ == '__main__':
     docker = True
     if docker:
         print("Docker is working")
-        workingPath = '\\Users\\Chaputa\\Documents\\Trier\\Master\\Masterarbeit\\rPPGCode'
+        workingPath = os.path.abspath(os.getcwd())
+        outputData = os.path.join(workingPath, "var",)
+        outputDataUBFCPath = os.path.join(outputData, "UBFCDataset")
+        outputDataWCDPath = os.path.join(outputData, "WCDDataset")
+        outputDataUBFCSplitPath = os.path.join(outputData, "UBFCDatasetSplit")
+        outputDataWCDSplitPath = os.path.join(outputData, "WCDDatasetSplit")
+        model_path = os.path.join(outputData, "Model")
         #while True:
         #    time.sleep(1)
         #    print('Still working')
     else:
         workingPath = os.path.abspath(os.getcwd())
-    genPath = os.path.dirname(workingPath)
-    outputDataUBFCPath = os.path.join(genPath, "output", "UBFCDataset")
-    outputDataWCDPath = os.path.join(genPath, "output", "WCDDataset")
+        genPath = os.path.dirname(workingPath)
+        outputDataUBFCPath = os.path.join(genPath, "output", "UBFCDataset")
+        outputDataWCDPath = os.path.join(genPath, "output", "WCDDataset")
+        outputDataUBFCSplitPath = os.path.join(genPath, "output", "UBFCDatasetSplit")
+        outputDataWCDSplitPath = os.path.join(genPath, "output", "WCDDatasetSplit")
+        model_path = os.path.join(genPath, "output", "Model")
     nFramesVideo = 128  # number of Frames used fpr training Model
-    #############################
+    #%%
     #       Preprocessing UBFC Dataset
     #PreprocessingUBFCMain.preprocessing_ubfc_dataset(genPath, nFramesVideo)
-    #############################
 
-    #############################
+
+    #%%
     # Preprocessing WCD Dataset
     #PreprocessingWCDMain.preprocessing_wcd_dataset(genPath, nFramesVideo)
-    #############################
 
-    #############################
+
+    #%%
     #       Split and load data
     # UBFC
     #splitData.split_data(outputDataUBFCPath, nFramesVideo)
-    outputDataUBFCSplitPath = os.path.join(genPath, "output", "UBFCDatasetSplit")
     training_loader, validation_loader, test_loader = loadData.load_data(outputDataUBFCSplitPath, nFramesVideo)
-
+    
     # WCD
     # splitData.split_data(outputDataWCDPath, nFramesVideo)
-    # outputDataWCDSplitPath = os.path.join(genPath, "output", "WCDDatasetSplit")
     # training_loader, validation_loader, test_loader = loadData.load_data(outputDataWCDSplitPath, nFramesVideo)
-    #############################
 
-    #############################
+
+    #%%
     #       Train and validate with PhysNet Model
     Plot_results = True
     test_loader = trainMain.train_model(outputDataUBFCPath, Plot_results, training_loader, validation_loader, test_loader)
-    #############################
 
-    #############################
+
+    #%%
     #       Test PhysNet Model
     Plot_results = True
-    model_path = os.path.join(genPath, "output", "Model")
+
     Testmain.test_model(model_path, test_loader, Plot_results)
-    ############################
