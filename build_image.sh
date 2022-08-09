3e2a830bb807aad1a7fc00af1bf04d11435cfe47
Laurens Kreilinger <laurens.kreilinger@web.de>
2022-08-09 13:34:21 +0200

    update

--- build_image.sh
+++ build_image.sh
@@ -4,7 +4,6 @@ export GID=$(id -g)
 docker build --build-arg USER=$USER \
              --build-arg UID=$UID \
              --build-arg GID=$GID \
-             --build-arg PW=29653847 \
              -t masterarbeitkreilinger:1.0 \
              .
 
