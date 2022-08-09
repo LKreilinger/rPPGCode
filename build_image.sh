ff3d9a947da8b5619b96cd6f53490bed73329f9c
Laurens Kreilinger <laurens.kreilinger@web.de>
2022-08-09 11:44:55 +0200

    update

--- build_image.sh
+++ build_image.sh
@@ -7,3 +7,7 @@ docker build --build-arg USER=$USER \
              --build-arg PW=29653847 \
              -t masterarbeitkreilinger:1.0 \
              .
+WANDB_API_KEY=b2b87b7a47a54d74a79cf8ceb131c26efe9418a5
+WANDB_NAME="My first run"
+WANDB_NOTES="Smaller learning rate, more regularization."
+WANDB_ENTITY=kreilinger
