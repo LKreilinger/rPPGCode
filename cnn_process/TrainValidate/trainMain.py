b71534eed6e668fc86eba10dd248d79392ffd851
Laurens Kreilinger <laurens.kreilinger@web.de>
2022-08-17 11:30:41 +0200

    gpu memory

--- cnn_process/TrainValidate/trainMain.py
+++ cnn_process/TrainValidate/trainMain.py
@@ -21,7 +21,6 @@ def train_and_validate_model(model, train_loader, validation_loader, loss_Inst, 
 
     Plot_results = False
     wandb.watch(model, loss_Inst, log="all", log_freq=10)
-    print(torch.distributed.get_world_size())
     print("cuda memory befor training")
     print(torch.cuda.memory_summary(device=config.device, abbreviated=False))
     # %% train and validate model
