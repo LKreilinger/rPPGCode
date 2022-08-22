08fcc9e66c6bcac2462aa045a3fbfc5028d4158d
Laurens Kreilinger <laurens.kreilinger@web.de>
2022-08-22 16:57:08 +0200

    add torch~=1.9.0

--- build_image.sh
+++ build_image.sh
@@ -5,7 +5,7 @@ docker build --build-arg USER=$USER \
              --build-arg UID=$UID \
              --build-arg GID=$GID \
              --build-arg PW=29653847 \
-             -t masterarbeit_kreilinger_pre_train_rppg:1.0 \
+             -t masterarbeit_kreilinger_train_rppg:1.0 \
              .
 
 
