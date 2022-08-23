f37137d9e9b9b8ee52715f95bab2cad669f4c675
Laurens Kreilinger <laurens.kreilinger@web.de>
2022-08-23 11:12:42 +0200

    torch.cuda

--- __main__.py
+++ __main__.py
@@ -62,11 +62,19 @@ if __name__ == '__main__':
     # %%
     # Preprocessing UBFC_rPPG dataset
     seeds = [2, 3, 4]
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     print(torch.cuda.is_available())
     print(torch.cuda.device_count())
     print(torch.cuda.current_device())
     print(torch.cuda.device(0))
     print(torch.cuda.get_device_name(0))
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    print(torch.cuda.current_device())
+    print(torch.cuda.device(1))
+    print(torch.cuda.get_device_name(1))
+
     for seed in seeds:
         config_pre_UBFC_rPPG = dict(
             train_split=60,
