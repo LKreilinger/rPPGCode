9c857068af27e226baf94e5e143042fcb1065111
Laurens Kreilinger <laurens.kreilinger@web.de>
2022-09-05 12:19:42 +0200

    update

--- build_image.sh
+++ build_image.sh
@@ -5,7 +5,7 @@ docker build --build-arg USER=$USER \
              --build-arg UID=$UID \
              --build-arg GID=$GID \
              --build-arg PW=29653847 \
-             -t masterarbeit_kreilinger_cnn_rppg:1.0 \
+             -t masterarbeit_kreilinger_cnn_rppg_only_train:1.0 \
              .
 
 
