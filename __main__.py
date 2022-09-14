f95934f9469b719c9b986d953441004e0419bf3e
Laurens Kreilinger <laurens.kreilinger@web.de>
2022-09-14 17:26:06 +0200

    update

--- __main__.py
+++ __main__.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
         fps=30,
         nFramesVideo=n_FRAMES_VIDEO,
         device=device,
-        epochs=15,
+        epochs=7,
         batch_size=size,
         learning_rate=lr,
         dataset="UBFC_rPPG",
