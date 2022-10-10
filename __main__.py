9cc9454e19c42124a6ddfa21cbe4c5f3d01278b9
Laurens Kreilinger <laurens.kreilinger@web.de>
2022-10-10 15:02:36 +0200

    3

--- __main__.py
+++ __main__.py
@@ -15,15 +15,15 @@ from Preprocessing import preprocessing_ubfc_main, pre_config, config_dataset
 from Preprocessing.WCD import PreprocessingWCDMain
 from cnn_process.TestModel import test_wcd
 from Preprocessing.PURE import preprocessing_pure_main
-from cnn_process import train_rppg
+from cnn_process import train_rppg, train_pure
 
 if __name__ == '__main__':
-    docker = True
-    n_FRAMES_VIDEO = 128  # number of Frames used fpr training Model
+    docker = False
+    n_FRAMES_VIDEO = 128  # number of Frames used for training Model
     tempPathNofile, genPath, workingPath = pre_config.pre_config(docker)
-    config_pre_UBFC_Phys, config_pre_WCD, config_pre_UBFC_rPPG, config_pre_PURE = config_dataset.config_datasets(genPath, tempPathNofile, workingPath, n_FRAMES_VIDEO)
 
     # Preprocessing datasets
+    config_pre_UBFC_Phys, config_pre_WCD, config_pre_UBFC_rPPG, config_pre_PURE = config_dataset.config_datasets(genPath, tempPathNofile, workingPath, n_FRAMES_VIDEO)
     #preprocessing_ubfc_main.pre_ubfc(config_pre_UBFC_Phys)
     #preprocessing_ubfc_main.pre_ubfc(config_pre_UBFC_rPPG)
     #PreprocessingWCDMain.preprocessing_wcd_dataset(config_pre_WCD)
@@ -44,54 +44,16 @@ if __name__ == '__main__':
     epochs = 37
     n = "2"  # 2 train rPPG with augment
     augmentation = True
-    train_rppg.train_rppg(genPath, augmentation, n_FRAMES_VIDEO, device, batch, lr, epochs, n)
+    #train_rppg.train_rppg(genPath, augmentation, n_FRAMES_VIDEO, device, batch, lr, epochs, n)
+
     # 3 train PURE                          -> test rPPG and WCD (split subject and video)
+    epochs = 15
+    n = "3"  # 3 train PURE
+    train_pure.train_pure(genPath, n_FRAMES_VIDEO, device, batch, lr, epochs, n)
     # 4 train rPPG (augment) and PURE       -> test WCD (split subject and video)
 
 
-    # frequnz augmentation
-    # config_cnn_pure = dict(
-    #     path_dataset=os.path.join(genPath, "output", "PURE_Dataset"),
-    #     path_model=os.path.join(genPath, "output", "Model"),
-    #     fps=30,
-    #     nFramesVideo=n_FRAMES_VIDEO,
-    #     device=device,
-    #     epochs=10,
-    #     batch_size=size,
-    #     learning_rate=lr,
-    #     dataset="UBFC_rPPG",
-    #     architecture="PhysNet")
-    #
-    # model = cnn_process_main.cnn_process_main(config_cnn_pure)
-    # # %% Test model with UBFC rPPG data
-    # config_cnn_test_UBFC_rPPG = dict(
-    #     path_dataset=os.path.join(genPath, "output", "UBFC_rPPG_Dataset", "test"),
-    #     path_model=os.path.join(genPath, "output", "Model"),
-    #     variblesPath=os.path.join(genPath, "output", "noFaceList"),
-    #     nFramesVideo=n_FRAMES_VIDEO,
-    #     fps=30,
-    #     device=device,
-    #     batch_size=size,
-    #     subjects=2,
-    #     dataset="WCD",
-    #     architecture="PhysNet")
-    #
-    # test_wcd.test_model(config_cnn_test_UBFC_rPPG)
-    #
-    # # %% Test model with WCD data
-    # config_cnn_test_wcd = dict(
-    #     path_dataset=os.path.join(genPath, "output", "WCD_Dataset", "test"),
-    #     path_model=os.path.join(genPath, "output", "Model"),
-    #     variblesPath=os.path.join(genPath, "output", "noFaceList"),
-    #     nFramesVideo=n_FRAMES_VIDEO,
-    #     fps=30,
-    #     device=device,
-    #     batch_size=size,
-    #     subjects=2,
-    #     dataset="WCD",
-    #     architecture="PhysNet")
-    #
-    # test_wcd.test_model(config_cnn_test_wcd)
+
 
 
 
--- cnn_process/train_pure.py
+++ cnn_process/train_pure.py
@@ -4,14 +4,9 @@ from cnn_process import cnn_process_main
 from cnn_process.TestModel import test_wcd
 
 
-def train_rppg(genPath, augmentation, n_FRAMES_VIDEO, device, size, lr, epochs, n):
-    if augmentation:
-        dataset_name = "UBFC_rPPG_Dataset"
-    else:
-        dataset_name = "UBFC_rPPG_Dataset_no_aug"
-
-    config_cnn_ubfc_rppg = dict(
-        path_dataset=os.path.join(genPath, "output", dataset_name),
+def train_pure(genPath, n_FRAMES_VIDEO, device, size, lr, epochs, n):
+    config_cnn_pure = dict(
+        path_dataset=os.path.join(genPath, "output", "PURE_Dataset"),
         path_model=os.path.join(genPath, "output", "Model", n),
         fps=30,
         nFramesVideo=n_FRAMES_VIDEO,
@@ -22,10 +17,10 @@ def train_rppg(genPath, augmentation, n_FRAMES_VIDEO, device, size, lr, epochs, 
         dataset="UBFC_rPPG",
         architecture="PhysNet")
 
-    model = cnn_process_main.cnn_process_main(config_cnn_ubfc_rppg)
-    # Test model with PURE data
-    config_cnn_test_pure = dict(
-        path_dataset=os.path.join(genPath, "output", "PURE_Dataset", "test"),
+    model = cnn_process_main.cnn_process_main(config_cnn_pure)
+    # %% Test model with UBFC rPPG data
+    config_cnn_test_UBFC_rPPG = dict(
+        path_dataset=os.path.join(genPath, "output", "UBFC_rPPG_Dataset", "test"),
         path_model=os.path.join(genPath, "output", "Model", n),
         variblesPath=os.path.join(genPath, "output", "noFaceList", n),
         nFramesVideo=n_FRAMES_VIDEO,
@@ -36,9 +31,9 @@ def train_rppg(genPath, augmentation, n_FRAMES_VIDEO, device, size, lr, epochs, 
         dataset="WCD",
         architecture="PhysNet")
 
-    test_wcd.test_model(config_cnn_test_pure)
+    test_wcd.test_model(config_cnn_test_UBFC_rPPG)
 
-    #  Test model with WCD data
+    # %% Test model with WCD data
     config_cnn_test_wcd = dict(
         path_dataset=os.path.join(genPath, "output", "WCD_Dataset", "test"),
         path_model=os.path.join(genPath, "output", "Model", n),
@@ -50,10 +45,10 @@ def train_rppg(genPath, augmentation, n_FRAMES_VIDEO, device, size, lr, epochs, 
         subjects=2,
         dataset="WCD",
         architecture="PhysNet")
+
     list_split = os.listdir(config_cnn_test_wcd["path_dataset"])
     for folder in list_split:
         config_cnn_test_wcd["path_dataset"] = os.path.join(config_cnn_test_wcd["path_dataset"], folder)
         config_cnn_test_wcd["dataset"] = "WCD_" + folder
         test_wcd.test_model(config_cnn_test_wcd)
         config_cnn_test_wcd["path_dataset"] = os.path.join(genPath, "output", "WCD_Dataset", "test")
-
