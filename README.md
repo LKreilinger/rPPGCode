890cf586e6692d662354fc447660a597b1d3d1a0
Laurens Kreilinger <laurens.kreilinger@web.de>
2022-12-09 14:46:30 +0100

    title

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-# Deep learning-based contactless pulse rate measurement from mobile patients
+# Deep learning-enabled remote monitoring of pulse rate for versatile patients
 Deep learning-based remote photoplethysmography (rPPG): 
 how to extract pulse signal from video using deep learning tools 
-Source code of the master thesis titled "Deep learning-based contactless pulse rate measurement from mobile patients"
+Source code of the master thesis titled "Deep learning-enabled remote monitoring of
+pulse rate for versatile patients"
 ## Abstract
-A method for collecting data has been devised that uses a mobile and remote photoplethysmography (rPPG)
-setup to get mobile data from people using an RGB camera and a pulse sensor.  
-Through a deep learning method, the pulse rate is determined in a contactless manner 
-using the self-generated dataset and two publicly available datasets. Using one publicly 
-available dataset to train and test the PhysNet model yields state-of-the-art results. 
-Cross-dataset training and testing the model resulted in poorer results, similar to classical 
-machine learning methods. In summary, the presented method for rPPG is not yet suitable 
-for reliable pulse rate determination in mobile patients.
+A method of data collection has been devised that uses a mobile and remote photoplethysmography (PPG) setup to obtain image data from people monitored with an
+RGB camera and pulse sensor. Through deep learning, the pulse rate (PR) is determined in a contactless manner using a self-generated dataset and two publicly available
+datasets. This study shows that the PhysNet model performs well on its training dataset,
+which is the UBFC-rPPG dataset. However, validating deep learning models with crossdataset in this study indicates a weakness in the generalizability of the models. In
+summary, the method proposed for remote-photoplethysmography (rPPG) is yet to be
+suitable for the reliable determination of PR in versatile patients.
 ## Cite as
-Laurens Kreilinger. (2022). "Deep learning-based contactless pulse rate measurement from mobile patients". 
+Laurens Kreilinger. "Deep learning-enabled remote monitoring of pulse rate for versatile patients". 
 Master Thesis. Trier: Trier University of Applied Sciences and Rosenheim: Rosenheim University of Applied Sciences. 2022
--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 """
 Master Thesis
 Laurens Kreilinger B. Sc.
-Title: Deep learning-based contactless pulse rate measurement from mobile patients
+Title: Deep learning-enabled remote monitoring of pulse rate for versatile patients
 
 Information to change  venv.:
     Run in Terminal -> .\venv\Scripts\activate
@@ -23,10 +23,10 @@ from cnn_process import train_rppg, train_pure, train_pure_rppg
 
 if __name__ == '__main__':
     docker = True
-    n_FRAMES_VIDEO = 128  # number of Frames used for training Model
     tempPathNofile, genPath, workingPath = pre_config.pre_config(docker)
 
     # Preprocessing datasets
+    n_FRAMES_VIDEO = 128  # number of Frames used for training Model
     config_pre_UBFC_Phys, config_pre_WCD, config_pre_UBFC_rPPG, config_pre_PURE = config_dataset.config_datasets(genPath, tempPathNofile, workingPath, n_FRAMES_VIDEO)
     preprocessing_ubfc_main.pre_ubfc(config_pre_UBFC_Phys)
     preprocessing_ubfc_main.pre_ubfc(config_pre_UBFC_rPPG)
@@ -38,15 +38,15 @@ if __name__ == '__main__':
     batch = 8  # batch size
     lr = 0.0001  # learning rate
 
-    # 1 train rPPG                          -> test PURE and WCD (split subject and video)
+    # 1 train UBFC-rPPG                     -> test PURE and WCD (split subject and video)
     epochs = 7
-    n = "1"  # 1 train rPPG
+    n = "1"  # 1 train UBFC-rPPG
     augmentation = False
     train_rppg.train_rppg(genPath, augmentation, n_FRAMES_VIDEO, device, batch, lr, epochs, n)
 
-    # 2 train rPPG with augment             -> test PURE and WCD (split subject and video)
+    # 2 train UBFC-rPPG with augment        -> test PURE and WCD (split subject and video)
     epochs = 37
-    n = "2"  # 2 train rPPG with augment
+    n = "2"  # 2 train UBFC-rPPG with augment
     augmentation = True
     train_rppg.train_rppg(genPath, augmentation, n_FRAMES_VIDEO, device, batch, lr, epochs, n)
 
@@ -55,9 +55,9 @@ if __name__ == '__main__':
     n = "3"  # 3 train PURE
     train_pure.train_pure(genPath, n_FRAMES_VIDEO, device, batch, lr, epochs, n)
 
-    # 4 train rPPG (augment) and PURE       -> test WCD (split subject and video)
+    # 4 train UBFC-rPPG (augment) and PURE  -> test WCD (split subject and video)
     epochs = 9
-    n = "4"  # 4 train rPPG (augment) and PURE
+    n = "4"  # 4 train UBFC-rPPG (augment) and PURE
     train_pure_rppg.train_pure_rppg(genPath, n_FRAMES_VIDEO, device, batch, lr, epochs, n)
 
 
